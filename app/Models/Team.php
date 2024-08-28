75ae3b07e2a27762d89044b5f747b36d5e5d0c90
Hans-Star18 <agussuardiasa1231@gmail.com>
2024-08-29 00:11:34 +0800

    ✨ feat: unik slug

--- app/Models/Team.php
+++ app/Models/Team.php
@@ -3,13 +3,14 @@
 namespace App\Models;
 
 use App\Traits\Slug;
+use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Team extends Model
 {
-    use Slug;
+    // use Slug;
 
     protected $fillable = [
         'registration_id',
@@ -18,6 +19,28 @@ class Team extends Model
     ];
 
     protected $guarded = ['id'];
+
+    public static function boot(): void
+    {
+        parent::boot();
+        static::creating(function (Model $model) {
+            if ($model->getAttribute('slug') === null) {
+                $model->setAttribute('slug', $model->generateUniqueSlug($model->getAttributeValue('name')));
+            }
+        });
+    }
+
+    private function generateUniqueSlug(string $name): string
+    {
+        $slug = Str::slug($name);
+        $uniqueCode = $this->generateUniqueCode();
+        return $slug . '-' . $uniqueCode;
+    }
+
+    private function generateUniqueCode(): string
+    {
+        return Str::random(8);
+    }
 
     public function members(): HasMany
     {
